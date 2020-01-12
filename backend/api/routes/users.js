a6357ebaa244a4bcc711d676a2f86f092c9f69b6
Asela <aseladamithkumara1995@gmail.com>
2020-01-12 14:30:08 -0600

    Done the login part

--- backend/api/routes/users.js
+++ backend/api/routes/users.js
@@ -54,6 +54,58 @@ router.post('/signup', (req, res, next) => {
 
 });
 
+router.post('/login', (req, res, next) => {
+    console.log('sss');
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if(user){
+                console.log("user is not fuckiing herer")
+            }
+            if (user.length < 1) {
+                return res.status(401).json({
+                    "message": 'Auth failed'
+                });
+            }
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({
+                        message: 'Auth failed'
+                    });
+                }
+                if(result) {
+
+                    const token = jwt.sign({
+                        email: user[0].email,
+                        userId: user[0]._id
+                    }, 
+                    process.env.JWT_KEY,
+                    {
+                        expiresIn: "1h"
+                    }
+
+                    );
+                    return res.status(200).json({
+                        message: 'Auth successful',
+                        flag: true,
+                       
+                    });
+                }
+                    return res.status(401).json({
+                        message: 'Auth failed',
+                        flag : false
+                    });
+            });
+
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).json({
+                "error": err
+            });
+        });
+});
+
 router.delete('/:userId', (req, res, next) => {    User.remove({_id: req.params.userId})
     .exec()
     .then(result => {
