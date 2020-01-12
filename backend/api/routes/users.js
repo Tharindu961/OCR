ff3e3fd8022fe21c863104c81912803bd9e4ab27
Sandali Weththasinghe <37362261+SandaliWeththasinghe@users.noreply.github.com>
2020-01-12 14:05:58 +0530

    Did the signup part

--- backend/api/routes/users.js
+++ backend/api/routes/users.js
@@ -9,6 +9,52 @@ const Tesseract = require('tesseract.js');
 
 const User = require("../models/user");
 
+router.post('/signup', (req, res, next) => {
+    User.find({email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: 'Mail exists'
+                });
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                    const user = new User({
+                        _id: new mongoose.Types.ObjectId(),
+                        email: req.body.email,
+                        password: hash
+                    });
+                    user
+                        .save()
+                        .then(result => {
+                            console.log(result);
+                            res.status(201).json({
+                                message: 'User Created'
+                            });
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            res.status(500).json({
+                                error: err
+                            });
+                        });
+             }
+                
+            });
+
+            }
+        })
+        
+
+
+});
+
+
 
 
 router.delete('/:userId', (req, res, next) => {
